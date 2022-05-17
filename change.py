a5f2764fba6bce2964c78430dbb39faf4926d3a8
mztony <1287466969@qq.com>
2022-05-17 17:19:11 +0800

    Create change.py
    批量修改文件，在文件名前加入新内容

---
+++ change.py
@@ -0,0 +1,12 @@
+import os,sys                       
+def add_prefix_files(mark):             
+    old_names = os.listdir( path )  #获得文件夹中的文件名，生成list
+    for old_name in old_names:      
+            if  old_name!= sys.argv[0]:  #代码本身文件路径，防止脚本文件放在path路径下时，被一起重命名
+               if old_name.endswith('.jpg'):   #找到以.jpg 为后缀的文件,可改
+                    os.rename(os.path.join(path,old_name),os.path.join(path,mark+old_name))  #rename
+                    print (old_name,"has been renamed successfully! New name is: ",mark+old_name)  #rename success
+if __name__ == '__main__': 
+        path = r'/home/mozhe/test/fire_hydrant/JPEGImages/'   #运行程序前，记得修改主文件夹路径！
+        add_prefix_files('2007_')
+        
